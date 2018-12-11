81a8e1e28f2eb7aa9847358279c4feb982b3008f
Alexander Ilyukchin <iluch@mail.ru>
2018-12-11 17:40:25 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import { sum } from './modules/module1'
+console.log(sum(1, 5))
+let a = 12
+const b = 'Test'
+document.body.addEventListener('click', ({target}) => {
+  console.log(target)
+})
+import $ from 'jquery'
